b85502e4593a3d3fd7e14091ffa210dbb54102e5
Jay Miller <kjaymiller@gmail.com>
2021-10-02 18:32:37 +0000

    move files

--- ingest/connection.py
+++
@@ -1,7 +0,0 @@
-from elasticsearch import Elasticsearch
-import os
-
-client = Elasticsearch(
-	hosts=[os.environ.get("ES_HOST", 'locahost')], # for local instance
-	http_auth=['elastic', os.environ.get('ES_PWD')]
-	)
